bc0bcc65a849928bb0358a9a24caf142005675e8
Alex <alex@hellocial.com>
2018-12-26 18:29:49 +0800

    update

--- src/register.ts
+++ src/register.ts
@@ -4,6 +4,7 @@ export function register<T extends object>(actions: T) {
     return new Proxy(actions, {
         get(target, key) {
             if (target[key] instanceof Function) {
+                // TODO: 待改进
                 return function saga(p1?: any, p2?: any, p3?: any, p4?: any, p5?: any, p6?: any) {
                     if (arguments.length > 6) {
                         throw new Error(`function ${key as string} should be less than six parameters`)
